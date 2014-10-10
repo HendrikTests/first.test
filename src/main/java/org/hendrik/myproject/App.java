73791a465d901cec1cb918002f63553a2fa5f49f
hendrik <hendrik.teintang@gmail.com>
2014-10-10 17:54:28 +0200

    Some changes

--- src/main/java/org/hendrik/myproject/App.java
+++ src/main/java/org/hendrik/myproject/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello, hello, hello World! " );
+        System.out.println( "Some more hello!" );
     }
 }
