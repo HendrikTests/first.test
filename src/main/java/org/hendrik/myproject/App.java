d629710f1abf3bf4daf974018308c818122d70a4
hendrik <hendrik.teintang@gmail.com>
2014-10-13 14:33:42 +0200

    Fixed a bug

--- src/main/java/org/hendrik/myproject/App.java
+++ src/main/java/org/hendrik/myproject/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println( "Bug fix" );
         System.out.println( "Hello, hello, hello World! " );
         System.out.println( "Some more hello, hello!" );
         
